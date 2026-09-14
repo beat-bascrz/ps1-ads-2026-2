4089cf2b145b0cb11f9db81bda22e4720f6f0e37
beat-bascrz <bassicruzb@gmail.com>
2026-09-14 12:16:04 +0000

    (10/08) Criação do projeto e primeiro controller

---
+++ api/src/main/java/br/bea/fatecfranca/api/controllers/HelloController.java
@@ -0,0 +1,26 @@
+package br.bea.fatecfranca.api.controllers;
+
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+public class HelloController {
+
+
+
+
+   @GetMapping("/")
+   public String hello() {
+       return "API em funcionamento!";
+   }
+
+
+
+
+}
