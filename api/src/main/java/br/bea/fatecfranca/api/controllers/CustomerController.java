df73e85df3e67b66185f1d097184a8ba75936c67
beat-bascrz <bassicruzb@gmail.com>
2026-09-20 23:23:16 +0000

    (31/08) Início da operação do primeiro controller

--- api/src/main/java/br/bea/fatecfranca/api/controllers/CustomerController.java
+++ api/src/main/java/br/bea/fatecfranca/api/controllers/CustomerController.java
@@ -1,7 +1,10 @@
 package br.bea.fatecfranca.api.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +31,12 @@ public class CustomerController {
                 .status(HttpStatus.CREATED)
                 .body(savedCustomer);
     }
+
+    @GetMapping
+    public List<Customer> findAll() {
+        return repository.findAll();
+    }
+
 
 
 }
